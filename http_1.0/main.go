0df0abfca093c4bbf77909678479a903fe9dba0a
st-ry <stry.makorepe@gmail.com>
2024-05-25 15:51:37 +0900

    update: http get client sample to devide process of logging

--- http_1.0/main.go
+++ http_1.0/main.go
@@ -7,17 +7,23 @@ import (
 )
 
 func main(){
-	resp, err := http.Get("http://localhost")
+	resp, err := http.Get("http://localhost:8888")
 	if err != nil {
 		panic(err)
 	}
 	
 	defer resp.Body.Close()
-	body, err := io.ReadAll(resp.Body)
-	
+	printHttpResponse(resp)
+}
+
+func printHttpResponse (response *http.Response){
+	log.Println("Status:", response.Status)
+	log.Println("StatusCode:", response.StatusCode)
+	body, err := io.ReadAll(response.Body)
 	if err != nil {
 		panic(err)
 	}
-	
-	log.Println(string(body))
+	log.Println("Body:",string(body))
+	log.Println("Fields:", response.Header)
+	log.Println("Content-Length:", response.Header.Get("Content-Length"))
 }
